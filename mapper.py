7df1c4ffd2593e903107c5bf5146a34b43818302
Hussein Elhawary <husseinelhawary@gmail.com>
2024-05-06 22:48:26 +0300

    Initializing mapper file Co-authored-by: Moaaz Tarek <moa234@users.noreply.github.com>

---
+++ mapper.py
@@ -0,0 +1,64 @@
+'''
+This file contains the functions that map the input bits to the corresponding symbols.
+'''
+
+def coh_bpsk_mapper():
+    '''
+    This function maps the input bits to the corresponding BPSK symbols.
+    '''
+    print("not implemened")
+
+def qbsk_mapper():
+    '''
+    This function maps the input bits to the corresponding QPSK symbols.
+    '''
+    print("not implemened")
+
+def psk_8_mapper():
+    '''
+    This function maps the input bits to the corresponding 8-PSK symbols.
+    '''
+    print("not implemened")
+
+def coh_ook_mapper():
+    '''
+    This function maps the input bits to the corresponding OOK symbols.
+    '''
+    print("not implemened")
+
+def coh_ask_8_mapper():
+    '''
+    This function maps the input bits to the corresponding 8-ASK symbols.
+    '''
+    print("not implemened")
+
+def qam_mapper():
+    '''
+    This function maps the input bits to the corresponding QAM symbols.
+    '''
+    print("not implemened")
+
+def qam_8_mapper():
+    '''
+    This function maps the input bits to the corresponding 8-QAM symbols.
+    '''
+    print("not implemened")
+
+def qam_16_mapper():
+    '''
+    This function maps the input bits to the corresponding 16-QAM symbols.
+    '''
+    print("not implemened")
+
+def coh_bfsk_mapper():
+    '''
+    This function maps the input bits to the corresponding BFSK symbols.
+    '''
+    print("not implemened")
+
+def diff_bpsk_mapper():
+    '''
+    This function maps the input bits to the corresponding differentially encoded BPSK symbols.
+    '''
+    print("not implemened")
+
