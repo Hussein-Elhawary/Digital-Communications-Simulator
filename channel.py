62a0a4747fd243ee8893006981b0a712533adf73
Omar Elzahar <omarshelzahar@gmail.com>
2024-05-16 00:41:17 +0300

    clean channel.py

--- channel.py
+++ channel.py
@@ -18,13 +18,6 @@ def Nsymbols(Es, No):
 def noise_signal(Ns, size):
     return np.random.randn(size) * np.sqrt(Ns / 2) + 1j * np.random.randn(size) * np.sqrt(Ns / 2)
 
-# def generate_channel(nb, avg_energy, size):
-#     Eavg = energy_average(nb)
-#     Es = energy_symbol(nb, avg_energy)
-#     No = Nsymbols(Es, 1)
-#     Ns = No * Es
-#     noise_signal = noise(Ns, size)
-#     return noise_signal
 
 def generateVariance(complex_bits, noiseValue, nb):
     No = 10 **  (-noiseValue / 10)
@@ -36,47 +29,3 @@ def generate_channel(complex_bits, noiseValue, nb):
     No_over_2 = generateVariance(complex_bits, noiseValue, nb)
     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
     return complex_bits + noise_signal
-
-# def coh_bpsk_channel(complex_bits, noiseValue):
-#     No_over_2 = generateVariance(complex_bits, noiseValue, 1)
-#     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
-#     return complex_bits + noise_signal
-
-# def qpsk_channel(complex_bits, noiseValue):
-#     No_over_2 = generateVariance(complex_bits, noiseValue, 2)
-#     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
-#     return complex_bits + noise_signal
-
-# def psk_8_channel(complex_bits, noiseValue):
-#     No_over_2 = generateVariance(complex_bits, noiseValue, 3)
-#     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
-#     return complex_bits + noise_signal
-
-# def coh_ook_channel(complex_bits, noiseValue):
-#     No_over_2 = generateVariance(complex_bits, noiseValue, 1)
-#     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
-#     return complex_bits + noise_signal
-
-# def coh_ask_8_channel(complex_bits, noiseValue):
-#     No_over_2 = generateVariance(complex_bits, noiseValue, 3)
-#     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
-#     return complex_bits + noise_signal
-
-# def qam_channel(complex_bits, noiseValue):
-#     No_over_2 = generateVariance(complex_bits, noiseValue, 4)
-#     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
-#     return complex_bits + noise_signal
-
-# def qam_8_channel(complex_bits, noiseValue):
-#     No_over_2 = generateVariance(complex_bits, noiseValue, 3)
-#     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
-#     return complex_bits + noise_signal
-
-# def qam_16_channel(complex_bits, noiseValue):
-#     No_over_2 = generateVariance(complex_bits, noiseValue, 4)
-#     noise_signal = np.random.randn(len(complex_bits)) * np.sqrt(No_over_2) + 1j * np.random.randn(len(complex_bits)) * np.sqrt(No_over_2)
-#     return complex_bits + noise_signal
-
-
-
-
